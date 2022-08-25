e228953920572a94fae74d8877d824c849516287
Abdiel Alejandro Ramírez Barrón <a01236062@chi-1115-21.svcs.itesm.mx>
2022-08-24 18:28:18 -0600

    second commit from Computer 2`

---
+++ Desktop/PPT/PPT/PPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PPTApp.swift
+//  PPT
+//
+//  Created by Abdiel Alejandro Ramírez Barrón on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
